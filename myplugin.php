de9c62bac5ea60167a68da17f409b696d8afc28c
Ashimi <princeadebowale3@gmail.com>
2020-08-26 17:40:43 +0100

    Simple plugin created

--- myplugin.php
+++ myplugin.php
@@ -1,6 +1,19 @@
 <?php
 /**
  * Plugin name: Ashimi's First Plug
+ * Author: <a href="https://ashimi.xyz" target="_blank">Ashimi Abdulwahab</a>
  * Description: Ashimi playing around. lol.
- */
+ * Version: 1
+ * 
+**/
+
+    function plug_function()
+    {
+        $info = "Trying new stuffs out";
+        $info .= "<div  style='color:green;'>We are building Cool stuff!!!</div>";
+        $info .= "<div  style='color:red;'>We are building Cool stuff!!!</div>";
+        $info .= "<div  style='color:yellow;'>We are building Cool stuff!!!</div>";
+        return $info;
+    }
+    add_shortcode('example', 'plug_function');
 ?>
